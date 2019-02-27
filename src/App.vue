db8fbd91eb230fd9f8da0c26e9808207aeb21907
lmmendonca <lmaximino.mendonca@gmail.com>
2019-02-27 11:22:00 -0300

    inicio

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="corpo">
+    <meu-menu :rotas="routes"/>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+  import Menu from './components/shared/menu/Menu.vue'
+  import {routes} from "./routes";
+
+  export default {
+    components: {
+      'meu-menu': Menu
+    },
+
+    data() {
+      return {
+        routes
+      }
+    }
+  }
+</script>
+
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity 1s;
+  }
+</style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{titulo}}</h1>
+    <input type="serach" class="filtro" v-on:input="filtro = $event.target.value"
+           placeholder="filtre por parte do titulo">{{ filtro }}
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+          <meu-botao tipo="button" rotulo="REMOVER" @click="remove"/>
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Painel from '../shared/painel/Painel';
+  import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva';
+  import Botao from '../shared/botao/Botao';
+
+  export default {
+
+    components: {
+      'meu-painel': Painel,
+      'imagem-responsiva': ImagemResponsiva,
+      'meu-botao': Botao
+
+    },
+
+    data() {
+      return {
+        titulo: 'alura pic',
+        fotos: [],
+        filtro: ''
+      }
+    },
+
+    computed: {
+      fotosComFiltro() {
+
+        if (this.filtro || !this.filtro) {
+          let exp = new RegExp(this.filtro.trim(), 'i');
+          return this.fotos.filter(foto => exp.test(foto.titulo));
+        } else {
+          this.fotos;
+        }
+      }
+    },
+
+    methods: {
+      remove() {
+        alert('Remover a foto!')
+      }
+    },
+
+    created() {
+      this.$http.get('http://localhost:3000/v1/fotos')
+        .then(res => res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err));
+    }
+  }
+
+</script>
+
+<style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+
+</style>
